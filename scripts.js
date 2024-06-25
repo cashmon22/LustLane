f283dafbf9d776d23d44a65d2e4094a205faa897
cashmon22 <131389322+cashmon22@users.noreply.github.com>
2024-06-25 14:15:42 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// scripts.js
+document.getElementById('hamburger').addEventListener('click', function() {
+    var nav = document.getElementById('navigation');
+    nav.classList.toggle('show');
+});
